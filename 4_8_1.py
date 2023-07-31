b96dd18201912cbbc2eef20e0808cefb9199a1b4
Bompick <r.a.bayramov@gmail.com>
2023-07-31 22:11:28 +0300

    4_8_1 works_correct

--- 4_8_1.py
+++ 4_8_1.py
@@ -1,6 +1,6 @@
 class Vertex:
     def __init__(self):
-        self._links: [Link] = []
+        self._links = []
 
     @property
     def links(self):
@@ -13,11 +13,6 @@ class Link:
         self._v2 = v2
         self._dist = dist
 
-    def __eq__(self, other):
-        if not isinstance(other, self.__class__):
-            raise TypeError('Попытка сравненения объектов разных  классов')
-
-        return {self.v1, self.v2} == {other.v1, other.v2}
 
     @property
     def v1(self):
@@ -34,6 +29,12 @@ class Link:
     @dist.setter
     def dist(self, val):
         self._dist = val
+
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            raise TypeError('Попытка сравненения объектов разных  классов')
+
+        return {self.v1, self.v2} == {other.v1, other.v2}
 
 
 class LinkedGraph:
@@ -54,53 +55,40 @@ class LinkedGraph:
             for item in vertexes:
                 item.links.append(link)
 
-    def recurs(self, items, stop, path=[], global_path=[]):
+    def recurs(self, items, stop):
         for item in items:
-            path.append(item)
+            self.temp_path.append(item)
             all_smej_one_incl_item = list()
             for i in [x for row in [(val.v1, val.v2) for val in item.links] for x in row]:
                 if i not in all_smej_one_incl_item:
                     all_smej_one_incl_item.append(i)
-            all_smej_next = [j for j in all_smej_one_incl_item if j not in path]
+            all_smej_next = [j for j in all_smej_one_incl_item if j not in self.temp_path]
             if stop not in all_smej_next:
                 self.recurs(all_smej_next, stop)
-            # print(f'im here: {item}')
-            correct_route = path + [stop]
-            # print(correct_route)
-            path.remove(item)
+            correct_route = self.temp_path + [stop]
+            self.temp_path.remove(item)
             if stop in all_smej_next:
-                global_path.append(correct_route)
-
-        return global_path
+                self.global_path.append(correct_route)
+        return self.global_path
 
     def find_path(self, start_v, stop_v):
-        # prom = self.recurs([start_v], stop_v)
-        # print(prom)
-        # a = list(prom)
-        # print(a)
+        self.temp_path = []
+        self.global_path = []
         ttl_routes = list()
         ttl_routes.append(self.recurs([start_v], stop_v))
         ttl_routes = ttl_routes[0]
-        # print(ttl_routes)
         ttl_links = list(map(self.check, ttl_routes))
-        # print(ttl_links)
         ttl_times = list(map(self.find_time, ttl_links))
         ttl_times = list(map(sum, ttl_times))
         min_time_number = ttl_times.index(min(ttl_times))
-        # print(ttl_times)
-        # print(min_time_number)
+        res = ttl_routes[min_time_number]
         return ttl_routes[min_time_number], ttl_links[min_time_number]
-
-
-
-
 
     def find_time(self, massive):
         time = []
         for item in massive:
             time.append(item._dist)
         return time
-
 
     def check(self, massiv):
         route = []
@@ -110,14 +98,11 @@ class LinkedGraph:
                     route.append(item)
         return route
 
-
     def find_link(self, st1, st2, link):
         if st1 in link.__dict__.values() and st2 in link.__dict__.values():
             return link
         else:
             pass
-
-
 
 
 class Station(Vertex):
@@ -137,37 +122,49 @@ class LinkMetro(Link):
         super().__init__(v1, v2, dist)
 
 
-map_metro = LinkedGraph()
-v1 = Station("Сретенский бульвар")
-v2 = Station("Тургеневская")
-v3 = Station("Чистые пруды")
-v4 = Station("Лубянка")
-v5 = Station("Кузнецкий мост")
-v6 = Station("Китай-город 1")
-v7 = Station("Китай-город 2")
+map2 = LinkedGraph()
+v1 = Vertex()
+v2 = Vertex()
+v3 = Vertex()
+v4 = Vertex()
+v5 = Vertex()
 
-map_metro = LinkedGraph()
-v1 = Station("Сретенский бульвар")
-v2 = Station("Тургеневская")
-v3 = Station("Чистые пруды")
-v4 = Station("Лубянка")
-v5 = Station("Кузнецкий мост")
-v6 = Station("Китай-город 1")
-v7 = Station("Китай-город 2")
+map2.add_link(Link(v1, v2))
+map2.add_link(Link(v2, v3))
+map2.add_link(Link(v2, v4))
+map2.add_link(Link(v3, v4))
+map2.add_link(Link(v4, v5))
 
-map_metro.add_link(LinkMetro(v1, v2, 1))
-map_metro.add_link(LinkMetro(v2, v3, 1))
-map_metro.add_link(LinkMetro(v1, v3, 1))
+assert len(map2._links) == 5, "неверное число связей в списке _links класса LinkedGraph"
+assert len(map2._vertex) == 5, "неверное число вершин в списке _vertex класса LinkedGraph"
 
-map_metro.add_link(LinkMetro(v4, v5, 1))
-map_metro.add_link(LinkMetro(v6, v7, 1))
+map2.add_link(Link(v2, v1))
+assert len(map2._links) == 5, "метод add_link() добавил связь Link(v2, v1), хотя уже имеется связь Link(v1, v2)"
 
-map_metro.add_link(LinkMetro(v2, v7, 5))
-map_metro.add_link(LinkMetro(v3, v4, 3))
-map_metro.add_link(LinkMetro(v5, v6, 3))
+path = map2.find_path(v1, v5)
+s = sum([x.dist for x in path[1]])
+assert s == 3, "неверная суммарная длина маршрута, возможно, некорректно работает объект-свойство dist"
 
-print(len(map_metro._links))
-print(len(map_metro._vertex))
-path = map_metro.find_path(v1, v6)  # от сретенского бульвара до китай-город 1
-print(path[0])    # [Сретенский бульвар, Тургеневская, Китай-город 2, Китай-город 1]
-print(sum([x.dist for x in path[1]]))  # 7
+assert issubclass(Station, Vertex) and issubclass(LinkMetro, Link), "класс Station должен наследоваться от класса Vertex, а класс LinkMetro от класса Link"
+
+map2 = LinkedGraph()
+v1 = Station("1")
+v2 = Station("2")
+v3 = Station("3")
+v4 = Station("4")
+v5 = Station("5")
+
+map2.add_link(LinkMetro(v1, v2, 1))
+map2.add_link(LinkMetro(v2, v3, 2))
+map2.add_link(LinkMetro(v2, v4, 7))
+map2.add_link(LinkMetro(v3, v4, 3))
+map2.add_link(LinkMetro(v4, v5, 1))
+
+assert len(map2._links) == 5, "неверное число связей в списке _links класса LinkedGraph"
+assert len(map2._vertex) == 5, "неверное число вершин в списке _vertex класса LinkedGraph"
+
+path = map2.find_path(v1, v5)
+
+assert str(path[0]) == '[1, 2, 3, 4, 5]', path[0]
+s = sum([x.dist for x in path[1]])
+assert s == 7, "неверная суммарная длина маршрута для карты метро"
