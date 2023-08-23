f8afa9ae889d6e97770fdcf6f820d9a3aa50820b
Bompick <r.a.bayramov@gmail.com>
2023-08-23 09:11:38 +0300

    5_6_1_1 update

--- 5_6_1.py
+++ 5_6_1.py
@@ -1,4 +1,5 @@
 from random import randint
+
 class Ship:
     def __init__(self, length, tp=1, x=None, y=None):
         self._x = x
@@ -24,23 +25,58 @@ class Ship:
     def move(self, go):
         pass  # прописать функцию
 
-    def is_collide(self, ship):
+    def is_collide(self, ship=None):
+        if ship is None:
+            return True
+
         pass  # прописать функцию
 
-    def is_out_pole(self, size):
-        pass  # прописать функцию
+    def is_out_pole(self, size=10):
+        if self._tp == 1:
+            if self._x + self._length <= size - 1:
+                return True
+            else:
+                return False
+
+        elif self._tp ==2:
+            if self._y + self._length <= size -1:
+                return True
+            else:
+                return False
 
 
 class GamePole:
     def __init__(self, size=10):
         self._size = size
         self._ships = []
+        self._pole = [[0 for i in range(self._size)]
+                      for j in range(self._size)]
 
+    # def init(self):
+    #     for i in range(1, 5):
+    #         temp = [Ship(5-i, tp=randint(1, 2)) for j in range(i)]
+    #         self._ships.extend(temp)
 
     def init(self):
-        for i in range(1, 5):
-            temp = [Ship(5-i, tp=randint(1, 2)) for j in range(i)]
-            self._ships.extend(temp)
+        self._ships = [Ship(5 - i, tp=randint(1, 2))
+                       for i in range(1, 5)
+                       for j in range(i)]
+
+        for number, ship in enumerate(self._ships):
+            temp_ships_list=[]
+            while True:
+                x, y = [randint(0, self._size-1) for i in range(2)]
+                temp_ship = Ship(ship._length, ship._tp, x, y)
+                if temp_ship.is_out_pole():
+                    for t_sh in temp_ships_list:
+                        
+                    ship.set_start_coords(x,y)
+                    break
+            print(f'My coords is {x, y}')
+
+            if number == 0:
+                pass
+
 
 
 
@@ -51,13 +87,15 @@ class GamePole:
         pass
 
     def show(self):
-        pass
+        for item in self._pole:
+            print(item)
 
     def get_pole(self):
-        pass
-
+        return tuple(tuple(item) for item in self._pole)
 
 
 ship = Ship(4)
 pole = GamePole()
 pole.init()
+pole.show()
+
