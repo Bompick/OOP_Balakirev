842d8184d1cac1761c9872d7476af508c0f03570
Bompick <r.a.bayramov@gmail.com>
2023-09-27 13:16:29 +0300

    1_6_9_tests_added

--- 1 /1_6_9.py
+++ 1 /1_6_9.py
@@ -27,3 +27,21 @@ TYPE_OS =2
 e = Dialog("El")
 print(isinstance(p,DialogWindows))
 print(p.__dict__)
+
+
+
+
+TYPE_OS = 1
+dlg_1 = Dialog("123")
+TYPE_OS = 2
+dlg_2 = Dialog("1234")
+
+assert isinstance(dlg_1, DialogWindows) and isinstance(dlg_2, DialogLinux), "создаваемые объекты не соответствуют нужным классам DialogWindows или DialogLinux"
+
+assert dlg_1.name == "123", "неверное значение локального атрибута name класса DialogWindows"
+assert dlg_2.name == "1234", "неверное значение локального атрибута name класса DialogLinux"
+
+d1 = Dialog("12")
+d2 = Dialog("123")
+
+assert d1.name == "12" and d2.name == "123", "неверные значения в локальных атрибутах name разных объектов класса DialogLinux"
