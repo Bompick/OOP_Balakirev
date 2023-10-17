de8bb1ca7bfb496f673fce0b32379b8161157359
Bompick <r.a.bayramov@gmail.com>
2023-10-17 09:42:09 +0300

    2_1_11_tests_added

--- 2/2_1_11.py
+++ 2/2_1_11.py
@@ -39,3 +39,12 @@ class EmailValidator:
     
 
 print(EmailValidator.check_email('1@gmail@com'))
+
+assert EmailValidator.check_email("sc_lib@list.ru") == True and EmailValidator.check_email("sc_lib@list_ru") == False and EmailValidator.check_email("sc@lib@list_ru") == False and EmailValidator.check_email("sc.lib@list_ru") == False and EmailValidator.check_email("sclib@list.ru") == True and EmailValidator.check_email("sc.lib@listru") == False and EmailValidator.check_email("sc..lib@list.ru") == False, "метод check_email отработал некорректно"
+
+m = EmailValidator.get_random_email()
+assert EmailValidator.check_email(m) == True, "метод check_email забраковал сгенерированный email методом get_random_email"
+
+assert EmailValidator() is None, "при создании объекта класса EmailValidator возвратилось значение отличное от None"
+
+assert EmailValidator._EmailValidator__is_email_str('abc'), "метод __is_email_str() вернул False для строки"
